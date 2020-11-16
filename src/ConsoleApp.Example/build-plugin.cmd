68aaa00deaaaddea36b72b41b7c2b46149878ac8
xfrogcn <xfrogcn@163.com>
2020-11-16 11:31:40 +0800

    升级到.NET 5.0/PluginFactory 2.0.0

---
+++ src/ConsoleApp.Example/build-plugin.cmd
@@ -0,0 +1,4 @@
+dotnet publish ../../plugins/TestPluginA -o ./bin/Debug/net5.0/Plugins
+rm -f ./bin/Debug/net5.0/Plugins/Microsoft.*
+rm -f ./bin/Debug/net5.0/Plugins/Xfrogcn.PluginFactory.*
+rm -f ./bin/Debug/net5.0/Plugins/*.json
